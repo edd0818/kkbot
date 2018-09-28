bb40665b82b6ecced0f8a57bdf45aae34533005d
Eddie.Hsieh <eddie.hsieh@hy-tech.com.tw>
2018-09-28 17:10:41 +0800

    fix syntax error

--- run_bot.sh
+++ run_bot.sh
@@ -1,7 +1,7 @@
 #!/data/data/com.termux/files/usr/bin/bash
 cp ~/storage/downloads/kkbot-master/kkbot-master/kk_a.sh ~/
 sed -i -- 's/usr\/bin/data\/data\/com.termux\/files\/usr\/bin/g' kk_a.sh
-iconv -f utf-8 -tbig5 kk_a.sh > kk_a.exp
+iconv -f utf-8 -t big5 kk_a.sh > kk_a.exp
 chmod +x kk_a.exp
 LANG=zh_TW.BIG5 ~/kk_a.exp 
 
