a1abca265f31cf0af788a1423d33b70737a23a41
Eddie.Hsieh <eddie.hsieh@hy-tech.com.tw>
2018-09-29 12:06:30 +0800

    發生錯誤重新執行-4

--- run_bot_a.sh
+++ run_bot_a.sh
@@ -1,5 +1,5 @@
 #!/data/data/com.termux/files/usr/bin/bash
 cp ~/storage/downloads/kkbot-master/kkbot-master/$1 ~/
 sed -i -- 's/usr\/bin/data\/data\/com.termux\/files\/usr\/bin/g' $1
-~/run_bot.sh ~/$1.sh 
+~/run_bot.sh ~/$1
 
