3100296b55adae7b93d94d972caede48a443913c
Eddie.Hsieh <eddie.hsieh@hy-tech.com.tw>
2018-10-01 00:43:19 +0800

    mercy farming

--- kk_a.sh
+++ kk_a.sh
@@ -1,11 +1,40 @@
 #!/usr/bin/expect -f
 
+proc recall {} {
+    sleep 1
+    expect ">" {
+        puts "Recalling"
+        send "recall\r"
+        expect {
+            "你正忙著呢" {
+                recall
+            }
+        }
+    }
+    sleep 5
+}
+
 proc go { direct steps} {
     while {$steps > 0 } {
         expect {
             "你的動作還沒有完成, 不能移動" {
                 sleep 2
+                go $direct 1
+            }
+            "你走向如鏡面般的水藍色薄膜" {
+                sleep 2
                 exp_continue
+            }
+            "你小心翼翼地提腳向這片白色的薄膜走去" {
+                sleep 2
+                exp_continue
+            }
+            "你試圖逃走" {
+                sleep 1
+                go $direct 1
+            }
+            -re "你.*逃跑了" {
+                recall
             }
             ">"
             {
@@ -23,21 +52,19 @@ proc lookfor { target } {
     ">"
     {
         puts "looking for \[$target]."
-        send "l\r"
+        send "l $target\r"
         expect {
-                "Rabbitslayer" {
-                    puts "$target not found."
-                    return 0
-                }
-                "$target" 
-                {
+                -re "正處於" {
                     puts "\[$target] found."
                     return 1
                 }
-                default
-                {
+                "你要看什麼" {
                     puts "\[$target] not found."
                     return 0
+                }
+                default {
+                    puts "\[$target] found."
+                    return 1
                 }
             }
             
@@ -64,13 +91,21 @@ proc isDead { target } {
     }       
 }
 
+proc handleCorpse {} {
+    puts "Get all from corpse."
+    sleep 1
+    send "gc\r"
+    expect ">"
+    send "pa\r"
+}
+
 proc prepareToFight {} {
     set hp [getHP]
     set mp [getMP]
     sleep 1
     buffAll
 
-    if {$hp < 90 && $mp > 85} {
+    if {$hp < 90 && $mp > 70} {
         puts "Get heal to start fighting."
         sleep 1
         cast "cc"
@@ -96,27 +131,29 @@ proc kill { target count } {
                         return
                     }
                     "你喝道 :「可惡的" {
-                        set isTargetDead [isDead "$target"]
-                        while { !$isTargetDead } {
-
-                            set isTargetDead [isDead "$target"]
-
-                            if { $isTargetDead } {
-                                sleep 1
-                                puts "Get all from corpse."
-                                send "gc\r"
-                                expect ">"
-                                send "pa\r"
+                        expect {
+                            -re "死了|你得到.*點經驗" {
+                                puts "\[$target] is dead."
+                                handleCorpse
                                 set count [expr $count-1]
-                            } else {
+                            }
+                            -re "\[你|妳]?.*\[傷害|格開|但是沒中|從旁邊擦過|用盾擋開]" {
+                                set hasTarget [lookfor "$target"]
+                                puts "Fighting with \[$target]."
                                 # 戰鬥中補血
                                 set needHeal [expr ![isHealthy $heal_hp_limit] ]
 
                                 if {$needHeal} {
+                                    puts "Need healing in fighting."
                                     cast "ch"
                                 }
+                                exp_continue
                             }
-                            
+                            default {
+                                puts "\[$target] is dead."
+                                handleCorpse
+                                set count [expr $count-1]
+                            }
                         }
                     }
                 }
@@ -250,6 +287,13 @@ proc sellAll {} {
         puts "Inventory has been sold."
     }
 }
+
+proc transport { kingdom } {
+    expect ">" {
+        send "pray mercy\r"
+    }
+    sleep 5
+}
 #=====================================================================
 # Config
 #=====================================================================
@@ -259,7 +303,7 @@ set min_hp_limit 50
 # 高於血量停止休息
 set max_hp_limit 85
 # 戰鬥中低於血量，補血
-set heal_hp_limit 65
+set heal_hp_limit 70
 
 
 
@@ -275,96 +319,196 @@ send "xanver\r"
 expect "請輸入密碼"
 send "a77818\r"
 
-
-
 while {1} {
-    expect ">"
-    send "recall\r"
-
-    sleep 5
+    recall
 
     go "n" 1
-    go "e" 3
-    #kill "monk" 1
-    go "n" 1
-    kill "Barkeeper" 1
-    #kill "Adventurer" 2
-    go "s" 1
-    go "e" 1
-    kill "Priest" 2
-    kill "adventurer" 1
-    go "w" 1
-    go "s" 4
-    go "e" 1
-    sellAll
-    go "w" 1
-    go "s" 2
-    #城門
-    go "e" 1
-    go "n" 1   
-    #kill "Fox" 2
-    go "n" 1
-    go "e" 1
-    #kill "Deer" 4
-    go "e" 2
-    go "n" 2
-    kill "Buffalo" 3
-    go "e" 3
-    go "n" 1
-    kill "horse" 4
-    go "s" 1
-    go "w" 3
-    go "s" 2
-    go "w" 3
-    go "s" 2
-    go "e" 1
-    #kill "Rabbit" 3
-    go "s" 1
-    kill "Hunter" 1
-    go "s" 1
-    go "e" 2
-    go "s" 2
-    go "e" 2
-    #kill "Monkey" 3
-    go "w" 2
-    go "n" 2
-    go "w" 2
-    go "n" 2
-    go "w" 1
-    go "s" 2
-    kill "Adventurer" 2
     go "e" 4
-    go "s" 7
-    go "e" 2
-    kill "horse" 4
-    go "w" 2
-    go "n" 7
-    go "w" 4
-    go "w" 6
-    kill "willow" 1
-    go "e" 6
-    go "n" 2
-    go "w" 1
-    # 城門
-    kill "guard" 2
-    go "n" 2
+    kill "priest" 2
     go "e" 1
-    sellAll
+    transport mercy
+    go "e" 2
+    #精靈塔
+    sleep 2
+    go "u" 1
+    kill "spirit" 2
+    go "u" 1
+    kill "fairy" 2
+    go "u" 1
+    kill "element" 2
+    go "u" 1
+    kill "water" 1
+    go "d" 4
+    go "e" 1
+    kill "traveller" 1
+    kill "man" 1
+    go "e" 1
+    kill "man" 1
+    kill "girl" 1
+    go "e" 1
+    go "n" 1
+    kill "visitor" 1
+    kill "man" 1
+    go "s" 2
+    kill "pagan" 2
+    go "n" 1
+    go "e" 2
+    go "n" 1
+    kill "girl" 1
+    kill "traveller" 1
+    go "n" 1
+    kill "woman" 1
+    kill "girl" 1
+    kill "boy" 1
+    go "n" 1
+    kill "boy" 1
+    go "n" 2
+    go "u" 1
+    kill "spirit" 2
+    go "u" 1
+    kill "fairy" 2
+    go "u" 1
+    kill "element" 2
+    go "u" 1
+    kill "fire" 1
+    go "d" 4
+    go "s" 5
+    #主殿
+    go "s" 2
+    go "e" 1
+    kill "waitress" 1
     go "w" 1
     go "n" 2
-    go "w" 1
-    #kill "Guard" 2
-    go "w" 1
+    go "e" 2
     go "n" 1
-    #kill "Frog" 3
-    #go "s" 1
-    #go "w" 2
-    #go "n" 2
-    #go "e" 1
-    #go "s" 1
+    sellAll
+    kill "woman" 1
+    kill "clerk" 1
+    sellAll
+    go "s" 2
+    kill "adventurer" 1
+    kill "female" 1
+    #kill "Cleric" 1
+    go "n" 1
+    go "e" 3
+    go "u" 1
+     kill "spirit" 2
+    go "u" 1
+    kill "fairy" 2
+    go "u" 1
+    kill "element" 2
+    go "u" 1
+    kill "wind" 1
+    go "d" 4
+    #精靈塔
+    go "e" 1
+    sleep 2
+    go "e" 2
+    kill "child" 2
+    go "e" 1
+    kill "child" 2
+    go "e" 1
+    kill "child" 2
+    go "e" 1
+    kill "child" 2
+    go "n" 1
+    kill "child" 2
+
     sleep 1
     send "save\r"
 }
+
+
+#========[史坎布雷]=========
+
+# while {1} {
+#     expect ">"
+#     send "recall\r"
+
+#     sleep 5
+
+#     go "n" 1
+#     go "e" 3
+#     #kill "monk" 1
+#     go "n" 1
+#     kill "Barkeeper" 1
+#     #kill "Adventurer" 2
+#     go "s" 1
+#     go "e" 1
+#     kill "Priest" 2
+#     kill "adventurer" 1
+#     go "w" 1
+#     go "s" 4
+#     go "e" 1
+#     sellAll
+#     go "w" 1
+#     go "s" 2
+#     #城門
+#     go "e" 1
+#     go "n" 1   
+#     #kill "Fox" 2
+#     go "n" 1
+#     go "e" 1
+#     #kill "Deer" 4
+#     go "e" 2
+#     go "n" 2
+#     kill "Buffalo" 3
+#     go "e" 3
+#     go "n" 1
+#     kill "horse" 4
+#     go "s" 1
+#     go "w" 3
+#     go "s" 2
+#     go "w" 3
+#     go "s" 2
+#     go "e" 1
+#     #kill "Rabbit" 3
+#     go "s" 1
+#     kill "Hunter" 1
+#     go "s" 1
+#     go "e" 2
+#     go "s" 2
+#     go "e" 2
+#     #kill "Monkey" 3
+#     go "w" 2
+#     go "n" 2
+#     go "w" 2
+#     go "n" 2
+#     go "w" 1
+#     go "s" 2
+#     kill "Adventurer" 2
+#     go "e" 4
+#     go "s" 7
+#     go "e" 2
+#     kill "horse" 4
+#     go "w" 2
+#     go "n" 7
+#     go "w" 4
+#     go "w" 6
+#     kill "willow" 1
+#     go "e" 6
+#     go "n" 2
+#     go "w" 1
+#     # 城門
+#     kill "guard" 2
+#     go "n" 2
+#     go "e" 1
+#     sellAll
+#     go "w" 1
+#     go "n" 2
+#     go "w" 1
+#     #kill "Guard" 2
+#     go "w" 1
+#     go "n" 1
+#     #kill "Frog" 3
+#     #go "s" 1
+#     #go "w" 2
+#     #go "n" 2
+#     #go "e" 1
+#     #go "s" 1
+#     sleep 1
+#     send "save\r"
+# }
 
 exit
   
