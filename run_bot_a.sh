ce411208bcb719d8d5067007ae2be0c728cc05fa
Eddie.Hsieh <eddie.hsieh@hy-tech.com.tw>
2018-10-03 16:55:14 +0800

    手機改用git

--- run_bot_a.sh
+++ run_bot_a.sh
@@ -1,6 +1,4 @@
 #!/data/data/com.termux/files/usr/bin/bash
-cp ~/storage/downloads/kkbot-master/kkbot-master/$1 ~/
-cp ~/storage/downloads/kkbot-master/kkbot-master/lib.sh ~/
 sed -i -- 's/usr\/bin/data\/data\/com.termux\/files\/usr\/bin/g' $1
 ~/run_bot.sh ~/$1
 
